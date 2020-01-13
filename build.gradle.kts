baf9b0dcfb683ad44874921f057991d4d53a834c
Dmitriy Novikov <7nolikov@gmail.com>
2020-01-14 03:45:21 +0400

    Added spring dependencies to gradle build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("plugin.jpa") version "1.3.61"
     jacoco
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.isEnabled = true
-        csv.isEnabled = false
-        html.isEnabled = true
-        html.destination = file("$buildDir/reports/coverage")
-    }
 }
 
 repositories {
@@ -21,12 +16,37 @@ repositories {
 }
 
 dependencies {
+    // kotlin
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.statemachine:spring-statemachine-starter:2.1.2.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // utils
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("org.amshove.kluent:kluent:1.58")
+
+    // runtime
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        html.isEnabled = true
+        html.destination = file("$buildDir/reports/coverage")
+    }
 }
