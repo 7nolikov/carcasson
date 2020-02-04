696d027878e59867f0627bf41cc229e5f5b09a3e
Dmitriy Novikov <7nolikov@gmail.com>
2020-02-04 16:22:23 +0400

    Added draft of AuthClientTest.kt

---
+++ src/test/kotlin/carcassonne/controller/AuthClientTest.kt
@@ -0,0 +1,43 @@
+package carcassonne.controller
+
+import carcassonne.service.AuthService
+import carcassonne.statemachine.auth.AuthStateMachineFactory
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+
+class AuthClientTest {
+
+    @InjectMocks
+    private lateinit var authClient: AuthClient
+
+    @Mock
+    private lateinit var authService: AuthService
+
+    private val authStateMachine = AuthStateMachineFactory.getInstance()
+
+    @Test
+    fun testLogin() {
+        authClient.login(TEST_USERNAME, TEST_PASSWORD)
+    }
+
+    @Test
+    fun testRegister() {
+        authClient.register()
+    }
+
+    @Test
+    fun testLogout() {
+        authClient.logout()
+    }
+
+    @Test
+    fun testResetPassword() {
+        authClient.resetPassword()
+    }
+
+    companion object {
+        private const val TEST_USERNAME = "john"
+        private const val TEST_PASSWORD = "mysecretpassword"
+    }
+}
